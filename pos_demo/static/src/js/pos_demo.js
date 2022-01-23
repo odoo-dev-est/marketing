d79b46b4c387039470d3856514641696c70d7198
odoo-dev <odoo-dev@estelio.com>
2022-01-23 20:12:59 +0000

    [TEST] adding fetch to js

--- pos_demo/static/src/js/pos_demo.js
+++ pos_demo/static/src/js/pos_demo.js
@@ -1,1 +1,2 @@
-console.log('Point of Sale JavaScript loaded! This is a greeting!');
+/* console.log('Point of Sale JavaScript loaded! This is a greeting!');*/
+fetch('http://127.0.0.1:5000/products').then(data=> data.json()).then(response => console.log(response))
