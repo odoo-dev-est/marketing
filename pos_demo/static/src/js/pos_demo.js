592594bc1f9cfd94d7d49be45296812804df2c21
odoo-dev <odoo-dev@estelio.com>
2022-01-24 18:09:00 +0000

    [CHANGE] endpoint second time

--- pos_demo/static/src/js/pos_demo.js
+++ pos_demo/static/src/js/pos_demo.js
@@ -1,3 +1,3 @@
 /* console.log('Point of Sale JavaScript loaded! This is a greeting!');*/
 // fetch('http://127.0.0.1:5000/products').then(data=> data.json()).then(response => console.log(response))
-fetch('http://192.168.4.173:12376/').then(data=> data.json()).then(response => console.log(response))
+fetch('http://157.245.245.176:12376/').then(data=> data.json()).then(response => console.log(response))
