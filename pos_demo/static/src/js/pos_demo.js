72e398d5fbf0d135726814632274daf8be7bb398
odoo-dev <odoo-dev@estelio.com>
2022-01-27 13:12:33 +0000

    [EDIT] url endpoint

--- pos_demo/static/src/js/pos_demo.js
+++ pos_demo/static/src/js/pos_demo.js
@@ -1,8 +1,8 @@
-/* console.log('Point of Sale JavaScript loaded! This is a greeting!');*/
-// fetch('http://127.0.0.1:5000/products').then(data=> data.json()).then(response => console.log(response))
+console.log('Point of Sale JavaScript loaded! This is a greeting!');*/
+fetch('http://127.0.0.1:5000/products').then(data=> data.json()).then(response => console.log(response))
 // fetch('http://157.245.245.176:12376/').then(data=> data.json()).then(response => console.log(response))
 
-const item = {
+/* const item = {
       name: 'test_name',
       description: 'test_description',
       price: 1.7,
@@ -35,3 +35,4 @@ const item = {
           }).catch(e => {
             console.log(e);
           });
+          */
